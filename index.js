d853f6fdd8f90896bbbbbde9b9464b76d81f736c
igsourabh <sourabhvaish007@gmail.com>
2023-03-23 20:27:00 +0530

    Automated commit && git push origin main

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process');
 
 // Schedule a Git commit to run every day at 1:00 AM
 cron.schedule('* * * * *', () => {
-  exec('git add . && git commit -m "Automated commit && git push"', (error, stdout, stderr) => {
+  exec('git add . && git commit -m "Automated commit && git push origin main"', (error, stdout, stderr) => {
     if (error) {
       console.error(`Error: ${error}`);
       return;
