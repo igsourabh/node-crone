18f6a461029e647740772bb08ca992d17141c097
igsourabh <sourabhvaish007@gmail.com>
2023-04-01 13:52:41 +0530

    compress and push

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { exec } = require("child_process");
 // Schedule a Git commit to run every day at 1:00 AM
 
 cron.schedule("*/10 * * * * *", () => {
-  const filePath="example.txt"
+  const filePath = "backup";
   // Compress the file using tar and gzip
   const compressedFilePath = `${filePath}.tar.gz`;
   const compressCommand = `tar -czf ${compressedFilePath} ${filePath}`;
@@ -18,15 +18,22 @@ cron.schedule("*/10 * * * * *", () => {
       console.error(`Error compressing file: ${stderr}`);
       return;
     }
-    exec(
-      'git add . && git commit -m "Automated commit" && git push origin main',
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error: ${error}`);
-          return;
-        }
-        console.log(`Git commit successful1: ${stdout}`);
+    exec("git status", (error, stdout) => {
+      if (error) {
+        console.error(`Error: ${error}`);
+        return;
       }
-    );
+      console.log(`Git changes avilable `);
+      exec(
+        'git add backup.tar.gz && git commit -m "Automated commit" && git push origin main',
+        (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Error: ${error}`);
+            return;
+          }
+          console.log(`Git commit successful1: ${stdout}`);
+        }
+      );
+    });
   });
 });
