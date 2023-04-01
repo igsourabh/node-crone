ed72266558ac5731d6f0cda0f35e0d2ff09ceea2
igsourabh <sourabhvaish007@gmail.com>
2023-04-01 13:27:20 +0530

    Automated commit

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const cron = require('node-cron');
-const { exec } = require('child_process');
+const cron = require("node-cron");
+const { exec } = require("child_process");
 
 // Schedule a Git commit to run every day at 1:00 AM
-cron.schedule('*/10 * * * * *', () => {
-  exec('git add . && git commit -m "Automated commit" && git push origin main', (error, stdout, stderr) => {
+
+cron.schedule("*/10 * * * * *", () => {
+  const filePath="example.txt"
+  // Compress the file using tar and gzip
+  const compressedFilePath = `${filePath}.tar.gz`;
+  const compressCommand = `tar -czf ${compressedFilePath} ${filePath}`;
+
+  exec(compressCommand, (error, stdout, stderr) => {
     if (error) {
-      console.error(`Error: ${error}`);
-      return; 
+      console.error(`Error compressing file: ${error}`);
+      return;
     }
-    console.log(`Git commit successful1: ${stdout}`);
+    if (stderr) {
+      console.error(`Error compressing file: ${stderr}`);
+      return;
+    }
+    exec(
+      'git add . && git commit -m "Automated commit" && git push origin main',
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error: ${error}`);
+          return;
+        }
+        console.log(`Git commit successful1: ${stdout}`);
+      }
+    );
   });
-});        
+});
